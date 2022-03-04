c316ab7177d65ce2b96b25f6d20b927d8e22b914
verajesh <verajesh@deloitte.com>
2022-03-04 17:59:21 +0530

    change in webpack for scss compilation

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,38 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-const config = require('pwa-kit-react-sdk/webpack/config')
+// const Webpack = require('webpack')
+// const WebpackDevServer = require('webpack-dev-server')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-config[0].module.rules.push({
-    test: /\.scss$/,
-    use: [
-        'style-loader', //3. Inject styles into DOM
-        'css-loader', //2. Turns css into commonjs
-        'sass-loader' //1. Turns sass into css
-    ]
+let config = require('pwa-kit-react-sdk/webpack/config')
+let addSass = false
+
+// const complier = Webpack(config)
+config = config.map((root) => {
+    if (root?.module && root?.module?.rules.length && !addSass) {
+        root.module.rules.push({
+            test: /\.scss$/,
+            use: [
+                root.mode === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader, //4. Inject styles into DOM
+                'css-loader', //3. Turns css into commonjs
+                'postcss-loader', //2. Loader to process CSS
+                'sass-loader' //1. Turns sass into css
+            ]
+        })
+        addSass = true
+    }
+    return root
 })
+
+// config[0].module.rules.push({
+//     test: /\.scss$/,
+//     use: [
+//         'style-loader', //3. Inject styles into DOM
+//         'css-loader', //2. Turns css into commonjs
+//         'sass-loader' //1. Turns sass into css
+//     ]
+// })
 
 console.log('--------------------------Rajesh-----------')
 console.log(config[0].module.rules)
